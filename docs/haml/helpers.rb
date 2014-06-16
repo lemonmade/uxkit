85f1dbb1d543e8dc4d1a9a92c1c1138f70b42435
Chris Sauve <chris@pxldot.com>
2014-06-16 10:48:22 -0400

    Improved partials and helpers for rendering source code of components
    Signed-off-by: Chris Sauve <chris@pxldot.com>

--- docs/haml/helpers.rb
+++ docs/haml/helpers.rb
@@ -1,3 +1,16 @@
-def render(partial, locals = {})
-    Haml::Engine.new(File.read("docs/haml/partials/#{partial}.haml")).render(Object.new, locals)
+def render(file, locals = {})
+    Haml::Engine.new(File.read(file)).render(Object.new, locals)
+end
+
+def render_partial(partial, locals = {})
+    render "docs/haml/partials/#{partial}.haml", locals
+end
+
+def render_source_code(component)
+    component_file = component.downcase.gsub /\s+/, "-"
+    source_html = File.read("dist/components/#{component_file}/#{component_file}.html")
+    source_css = File.read("dist/components/#{component_file}/#{component_file}.css")
+    source_js = File.read("dist/components/#{component_file}/#{component_file}.js")
+
+    render_partial :source_code, html: source_html, css: source_css, js: source_js
 end
